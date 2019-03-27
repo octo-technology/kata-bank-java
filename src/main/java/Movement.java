b012d38d9b7df9f947fe063e846d008491a27b14
Mathieu Laurent <mlaurent@octo.com>
2019-03-27 16:25:39 +0100

    Direction String to Enum

--- src/main/java/Movement.java
+++ src/main/java/Movement.java
@@ -1,11 +1,17 @@
 class Movement {
+
     int amount;
-    String direction;
+    Direction direction;
     String date;
 
-    Movement(int amount, String direction, String date) {
+    Movement(int amount, Direction direction, String date) {
         this.amount = amount;
         this.direction = direction;
         this.date = date;
+    }
+
+    enum Direction {
+        DEPOSIT,
+        WITHDRAWAL
     }
 }
--- src/test/java/MovementTest.java
+++ src/test/java/MovementTest.java
@@ -6,10 +6,10 @@ public class MovementTest {
 
     @Test
     public void shouldHaveAmountDirectionAndDate() {
-        Movement movement = new Movement(100, "DEPOSIT", "2018-02-15");
+        Movement movement = new Movement(100, Movement.Direction.DEPOSIT, "2018-02-15");
 
         assertThat(movement.amount).isEqualTo(100);
-        assertThat(movement.direction).isEqualTo("DEPOSIT");
+        assertThat(movement.direction).isEqualTo(Movement.Direction.DEPOSIT);
         assertThat(movement.date).isEqualTo("2018-02-15");
     }
 }
